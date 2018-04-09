8eb8a3719d72fdc7bc635fad7808b77956b4d271
JT Cho <jonathan.cho@affirm.com>
2018-04-09 10:25:23 -0700

    Committing some more fun type things

---
+++ 21_1_Types.hs
@@ -0,0 +1,11 @@
+-- Defines a constructor function, NameConstr(String)
+data Name = NameConstr String
+data Color = ColorConstr String
+
+showInfos :: Name -> Color -> String
+showInfos (NameConstr name) (ColorConstr color) =
+    "Name: " ++ name ++ ", Color: " ++ color
+
+name = NameConstr "Robin"
+color = ColorConstr "Blue"
+main = putStrLn $ showInfos name color
---
+++ 21_Types.hs
@@ -0,0 +1,13 @@
+type Name = String
+type Color = String
+
+showInfos :: Name -> Color -> String
+showInfos name color = "Name: " ++ name ++ ", Color: " ++ color
+
+name :: Name
+name = "Robin"
+
+color :: Color
+color = "Blue"
+
+main = putStrLn $ showInfos name color
